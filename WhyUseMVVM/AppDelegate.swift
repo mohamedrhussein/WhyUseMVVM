a94a3026b3c601b8a8f6d10304437e0adb637e7a
Mohamed Rabie <mohamed.rabie@pharosholding.com>
2019-02-21 16:05:50 +0200

    initial pure project files

--- WhyUseMVVM/AppDelegate.swift
+++ WhyUseMVVM/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //start github using mvvm desgin patterns
+        
         return true
     }
 
